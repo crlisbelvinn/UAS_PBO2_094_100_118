cbf9dfd5d0cb1aa737167b99bc9f4215a046c217
Akhdanf11 <xpopow1104@gmail.com>
2023-05-27 12:03:45 +0700

    Memasukkan projek ke github

---
+++ src/main/java/com/mycompany/uas_labpbo/UAS_labpbo.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uas_labpbo;
+
+/**
+ *
+ * @author xpopow
+ */
+public class UAS_labpbo {
+
+    public static void main(String[] args) {
+        LoginFrame frame = new LoginFrame();
+        frame.setVisible(true);
+    }
+}
